a10d1cee23342b7528a514b8a4e83ce8404f8f1f
AdarshNaikDev <60536396+AdarshNaikDev@users.noreply.github.com>
2022-11-10 00:18:16 +0530

    pattern pgm added

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-console.log(" #")
-console.log(" ##")
-console.log(" ###")
-console.log("####")
+function staircase(n) {
+    
+  // want to declare a variable that will hold the final output that we will print 
+  let output = ''
+  
+  for (let i = 1; i <= n; i++) {
+  
+      for (let s = n - 1; s >= i; s--) {
+          output += ' '
+      }
+      
+      
+      for (let h = 1; h <= i; h++) {
+          output += '#'
+      }
+     
+      output += "\n"
+     
+  }
+ 
+  console.log(output)
+}
